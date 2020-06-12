df8eb57e286d09371ef1b515f02ed4decf60f97b
kwakyongkee <kyk0185@gmail.com>
2020-06-12 18:23:57 +0900

    Autopector publising Apply!

---
+++ src/Dummy.js
@@ -0,0 +1,179 @@
+export const data =[{
+  key:1,
+  agency:'서울시교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+},{
+  key:2,
+  agency:'경기교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+
+},{
+  key:3,
+  agency:'인천교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+
+},{
+  key:4,
+  agency:'대구교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+
+},{
+  key:5,
+  agency:'대전교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+
+},{
+  key:6,
+  agency:'광주교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+
+},{
+  key:7,
+  agency:'부산교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+
+},{
+  key:8,
+  agency:'울산교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+
+},{
+  key:9,
+  agency:'세종교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+
+},{
+  key:10,
+  agency:'강원교육청',
+  diffState:'Matching...',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-09',
+  scriptState:'점검중',
+  metaState:'점검중',
+
+},{
+  key:11,
+  agency:'경북교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+
+},{
+  key:12,
+  agency:'경남교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+
+},{
+  key:13,
+  agency:'전남교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+
+},{
+  key:14,
+  agency:'전북교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+
+},{
+  key:15,
+  agency:'충남교육청',
+  diffState:'Mismatch',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'Mismatch',
+
+},{
+  key:16,
+  agency:'충북교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+
+},{
+  key:17,
+  agency:'제주교육청',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+
+},{
+  key:18,
+  agency:'KERIS',
+  diffState:'Match',
+  scriptImg:'/img/db.svg',
+  metaImg:'/img/square.png',
+  udtDate:'2020-06-02',
+  scriptState:'OK',
+  metaState:'OK',
+}]
